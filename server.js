ea1e326652dd842b4abb72c42c700c5b3aefb6a4
Rene Santiago <rsj510@gmail.com>
2022-11-15 07:51:30 -0500

    added show route

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const budget = require('./models/budget')
+const PORT = 3000
 
 const app = express()
 
 app.use('/static', express.static('public'))
 
+// Index
 app.get('/', (req, res) => {
     res.render('index.ejs', {
         allBudget: budget,
     })
 })
 
-const PORT = 3000
+// Show
+app.get('/budget/:id', (req, res) => {
+    res.render('show.ejs', {
+        budget: budget[req.params.id]
+    })
+})
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
