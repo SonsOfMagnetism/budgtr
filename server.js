cd531cecfe0b8133ad1cdd3965fea4721a9e3def
Rene Santiago <rsj510@gmail.com>
2022-11-14 23:45:31 -0500

    applied data to index

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const budget = require('./models/budget')
 
 const app = express()
 
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        allBudget: budget,
+    })
 })
 
 const PORT = 3000
