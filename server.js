e93c1349f72c6626a67ba3b2e6e781eed28f293b
Rene Santiago <rsj510@gmail.com>
2022-11-15 14:29:16 -0500

    added create and new route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const budget = require('./models/budget')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const budget = require('./models/budget')
+
 const PORT = 3000
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use('/static', express.static('public'))
 
 // Index
@@ -17,6 +22,12 @@ app.get('/', (req, res) => {
 // New
 app.get('/budget/new', (req, res) => {
     res.render('new.ejs')
+})
+
+// Create
+app.post('/', (req, res) => {
+    budget.push(req.body)
+    res.redirect("/")
 })
 
 // Show
